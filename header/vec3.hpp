662c08cbb7ac54c2ce42826522da7f209481abe6
Cozy91 <suryanshguleria3@gmail.com>
2026-07-04 12:55:56 +0530

    arithimetic operations

--- header/vec3.hpp
+++ header/vec3.hpp
@@ -37,4 +37,97 @@ class vec3{ //for storing geometric vectors and colors
     inline vec3& operator /=(const vec3 &v2;
     inline vec3& operator *=(const float t);
     inline vec3& operator /=(const float t);
+
+    inline float length () const {
+    return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);} // to calculate magnitude of the 3d vector 
+    inline float squared_length() const{
+    return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];}
+    
+   inline void make_unit_vector();
+   float e[3];
+};
+
+inline std::istream& operator>>(std::istream& is,vec3 &t){ //directly reads a vector or vec3 object from an input stream like cin; example cin>>v
+is >> t.e[0] >> t.e[1] >> t.e[2]; //storing to vector , is is basically the source of the data
+return is; // returns is for chaining; basically for more than one use 
 }
+ inline std::ostream& operator<<(std::ostream& os,vec3 &t){
+ os<<t.e[0]<<t.e[1]<<t.e[2]; // writing from t to os for outpur on the screen, outputs all the three axis of vector together
+ return os;
+ }
+inline void vec3::make_unit_vector(){
+  float k=1.0/sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+  e[0] *= k; e[1] *= k; e[2] *= k; //vector upon its magnitude for unit vector 
+}
+inline vec3 operator+(const vec3 &v1,const vec3 &v2){
+  return vec3(v1.e[0] + v2.e[0],v1.e[1]+v2.e[1],v1[2]+v2.e[2]);
+}
+inline vec3 operator-(const vec3 &v1,const vec3 &vec2){
+  return vec3(v1.e[0]-v2.e[0],v1.e[1]-v2.e[1],v1.e[2]+v1.e[2]);
+}
+inline vec3 operator*(const vec3 &v1,const vec3 &v2){
+  return vec3(v1.e[0]*v2.e[0],v1.e[1]*v2.e[1],v1.e[2]*v2.e[2]);
+}
+inline vec3 operator/(const vec3 &v1,const vec3 &v2){
+  return vec3(v1.e[0]/v2.e[0],v1.e[1]/v2.e[1],v1.e[2]/v2.e[2]);
+}
+inline vec3 operator*(float t,const vec3 &v){
+  return vec3(t*v.e[0],t*v.e[1],t*v.e[2]);
+}
+inline vec3 operator/(vec3 v,float t){
+  return vec3(v.e[0]/t,v.e[1]/t,v.e[2]/t);
+}
+inline vec3 operator*(const vec3& v,float t){
+  return vec3(t*v.e[0],t*v.e[1],t*v.e[2]);
+}
+inline float dot(const vec3 &v1,const vec3 &v2){
+  return v1.e[0]*v2.e[0] + v1.e[1]*v1.e[1] + v2.e[2]*v2.e[2];
+}
+inline vec3 cross(const vec3& v1,const vec3& v2){
+  return vec3( (v1.e[1]*v2.e[0] - v1.e[2]*v2.e[1]),
+               (-(v1.e[0]*v2.e[2] - v1.e[2]*v2.e[0])),
+                (v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]) );
+}
+inline vec3& vec3::operator+=(const vec3 &v){
+  e[0] += v.e[0];
+  e[1] += v.e[1];
+  e[2] += v.e[2];
+  return *this
+}
+inline vec3& vec3::operator*=(const vec3 &v){
+  e[0] *= v.e[0];
+  e[1] *= v.e[1];
+  e[2] *= v.e[2];
+  return *this;
+}
+inline vec3& vec3::operator/=(const vec3& v){
+  e[0] /= v.e[0];
+  e[1] /= v.e[1];
+  e[2] /= v.e[2];
+}
+inline vec3& vec3::operator-=(const vec3& v){
+ e[0] -= v.e[0];
+ e[1] -= v.e[1];
+ e[2] -= v.e[2];
+ return *this;
+}
+inline vec3& vec3::operator*=(const float t){
+ e[0] *= t;
+ e[1] *= t;
+ e[2] *= t;
+ return *this;
+}
+inline vec3& vec3::operator/=(const float t){ //
+  float k=1.0/t; // doing this for better and faster performance 
+e[0] *= k;
+e[1] *= k;
+e[2] *= k;
+return *this;
+}
+inline vec3 unit_vector(vec3 v){ //not taking the reference here
+  return v/v.length();
+}
+
+
+
+
