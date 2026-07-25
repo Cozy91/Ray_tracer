432a93e1232cbf4bd36db01cab04ce97f2b9a43b
Cozy91 <suryanshguleria3@gmail.com>
2026-07-26 02:19:15 +0530

    antianalysing and a little diffuse materials

--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 #include "../header/vec3.hpp"
 #include "../header/ray.hpp"
-
+#include "../header/camera.hpp"
+#include "../header/sphere.hpp"
+#include "../header/hitable.hpp"
+#include "../header/hitablelist.hpp"
 
 // so now we want to find if a ray falls on a sphere or not(we are creating a sphere).A point on a ray or a line falls on the sphere if its coordiates satisfy the equation of the sphere. The point being p(t) as we mentioned earlier, so after substituting those values we get a quadratic equation with only t being the variable. THe solition of a quadratic equation is simple
 
@@ -12,14 +15,30 @@ bool hit_sphere(const vec3& center,float radius,const ray& r){
   float c = dot(oc,oc)-radius*radius; //  + c ; a*a + 2ab + c
   
   float discriminant= b*b - 4*a*c;
-  return(discriminant > 0); //dont want tangent cases; when the ray is just tangent to the sphere 
+  if(discriminant < 0){
+    return -1.0;
+  } //no solution if discriminant is less than 0 
+  else{
+    return (-b - sqrt(discriminant) / 2.0 *a) // finding roots formula 
+  }
+}
+
+vec3 random_in_unit_sphere() {
+  vec3 p;
+  do{
+    p = 2.0*vec3(drand48(),drand48(),drand48()) - vec3(1,1,1); // inside the range -1 to +1
+  } while(p.squared_length() >=1.0); //checking if ray is inside the unit sphere or nah 
+    return p;
 }
 
 vec3 color(const ray& r){
   // we make the image/background like this- top-blue;middle - bluish white; bottom-white 
-  if(hit_sphere(vec3(0,0,-1),0.5,r)){ // centre,radius and the ray 
-    return vec3(1,0,0); // if the ray hits the sphere, it returns red at vec(1,0,0);
-  }
+   float t =  hit_sphere(vec3(0,0,-1),0.5,r);// centre,radius and the ray ; t is the hitpoint 
+   if(t > 0.0){
+   vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1)); // normal for sphere at which the ray hits; making it a unit vector cuz we only care about its direction 
+     return 0.5*vec3(N.x()+1,N.y()+1,N.z()+1); // squashing the values between 0 to 1;
+   } // if the ray hits the sphere, it returns red at vec(1,0,0);
+  
   vec3 unit_direction = unit_vector(r.direction()); // direction of the unit vector defined in vec3 
    float t = 0.5*(unit_direction.y() + 1.0); // to make the range of t = 0 to 1, t is like a blend factor; to make the calculations easier 
    return (1.0-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0); // gives a combinaation of blue and white  depending on t,red is alway 1 so it doesnt matter muchj
@@ -28,18 +47,28 @@ vec3 color(const ray& r){
 int main(){
   int nx =200; //length of the screen
   int ny=100; // breadth of the screen
+  int ns=100; // number of pixels
   std::cout<<"P3\n"<<nx<<" "<<ny<<"\n255\n";
+  hitable* list[2];
+  list[0]= new sphere(vec3(0,0,-1),0.5);
+  list[1]= new sphere(vec3(0,-100.5,-1),0.5);
+  hitable* world=new hitable_list(list,2);
+  camera cam;
   vec3 lower_left_corner(-2.0,-1.0,-1.0);
   vec3 horizontal(4.0,0.0,0.0);
   vec3 vertical(0.0,2.0,0.0);
   vec3 origin(0.0,0.0,0.0);
   for(int j=ny-1;j>=0;j--){ //loop through rows
    for(int i=0;i<nx;i++){ //loop through columns in each row
-     // vec3 col(float(i)/float(nx),float(j)/float(ny),0.2); //red,green and blue(always 0.2)
-     float u=float(i)/float(nx);
-     float v=float(j)/float(ny);
-     ray r(origin,lower_left_corner+u*horizontal + v*vertical);
-     vec3 col=color(r);
+     //vec3 col(float(i)/float(nx),float(j)/float(ny),0.2); //red,green and blue(always 0.2)
+     vec3 col(0,0,0);
+     for(int s=0;s<ns;s++){
+       float u=float(i+drand48()) / float(nx);
+       float v=float(j+drand48()) / float(ny);
+       ray r=camera.get_ray(u,v); // one ray for one pixel, hence no of rays for no of pixelse 
+       col += color(r,world);
+     }    
+     col /= float(ns);
      int ir = int(255.99*col[0]);
      int ig = int(255.99*col[1]);
      int ib = int(255.99*col[2]);
